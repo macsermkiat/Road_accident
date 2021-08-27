5f6c7f6e17e2d9376c0fca3859c2aac637045d7f
Mac <macsermkiat@yahoo.com>
2021-08-27 21:44:51 +0700

    Add model and app

---
+++ app.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+@author: Sermkiat
+"""
+
+import numpy as np
+import pandas as pd
+import flask
+import pickle
+import joblib
+# app
+app = flask.Flask(__name__)
+
+# load model
+
+model = joblib.load('lr_rfe.pkl')
+#model = pickle.load(open("lr_rfe.pkl","rb"))
+
+
+
+# routes
+@app.route("/")
+def home():
+    return """
+           <body> 
+           <h1>Welcome to Road accident prediction<h1>
+           <a href="/page">Begin</a>
+           </body>"""
+
+    
+@app.route("/page")
+def page():
+    with open("page.html", 'r') as viz_file:
+        return viz_file.read()
+
+
+@app.route("/result", methods=["GET", "POST"])
+def result():
+    """Gets prediction using the HTML form"""
+    if flask.request.method == "POST":
+        inputs = flask.request.form
+        gender = inputs["gender"]
+        age = inputs["age"]
+        mechan = inputs["mechan"]
+        sbp = inputs["sbp"]
+        dbp = inputs["dbp"]
+        rr = inputs["rr"]
+        pr = inputs["pr"]
+        osat = inputs["osat"]
+        gcs = inputs["gcs"]
+    cols = ['gender', 'age', 'mechan', 'sbp', 'dbp', 'pr', 'rr', 'osat', 'gcs']
+    X_new = pd.DataFrame(np.array([(gender)] + [(age)] + [(mechan)]+
+                     [(sbp)] + [(dbp)] + [(pr)] + 
+                     [(rr)] + [(osat)] + [(gcs)]).reshape(1, -1), columns = cols)
+    
+    y_prob = model.predict_proba(X_new)
+    yhat = pd.Series(y_prob[0][1]).map(lambda x: 1 if x> 0.1537 else 0)
+    
+    #return "NeuralNetwork model predict outcome of " + outcome + "having seroma." 
+    return """<body><p> The prabability of death is """ + str(round(y_prob[0][1], 2)) + """</p>
+            <p>Predict """ + str(yhat[0]) + """</p></body>"""
+   
+
+if __name__ == '__main__':
+    """Connect to Server"""
+    HOST = "127.0.0.1"
+    PORT = "4000"
+    app.run(HOST, PORT, debug=True)
